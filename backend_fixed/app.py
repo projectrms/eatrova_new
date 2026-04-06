a693f3999442303535afb24729dfdb304f161ef0
Project RMS <project.rms.54321@gmail.com>
2026-04-06 10:56:18 +0530

    10:56

--- backend_fixed/app.py
+++ backend_fixed/app.py
@@ -1,4 +1,6 @@
 # app.py
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit, join_room, leave_room
